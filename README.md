5efa45c42245041aea933032f3705bdba10d4d6d
hoangthong0612 <hoangthongedm@gmail.com>
2022-03-17 10:06:22 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mlm
 MLM
+# thonga
---
+++ _db/shop.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `admins` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+--
+-- Đang đổ dữ liệu cho bảng `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `email`, `username`, `email_verified_at`, `image`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'hoangthongedm@gmail.com', 'admin', NULL, null, '$2y$10$pKtdDHznbfaiar0Ljzezbe/.fC68hkD8ccXh3MpWFk2c9zOa9sdmi', NULL, '2022-01-08 10:17:55');
+
+-- --------------------------------------------------------
